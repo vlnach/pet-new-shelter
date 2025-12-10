df09a0d90ffb374306a853fc2e229f54ff5e742d
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-12-10 19:45:49 +0100

    feat: refactor Header component and styles, move to frontend directory

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './styles.css'
 
-import Header from '@/components/Header/Component'
+import Header from '@/app/(frontend)/Header/Component'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
