2c1cb402efec6fdb034b04b5c1a6acdcc220cd62
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-12-19 18:21:34 +0100

    feat: add authToken to database configuration for improved security

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -31,6 +31,8 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URI || '',
+
+      authToken: process.env.DATABASE_AUTH_TOKEN,
     },
   }),
   sharp,
