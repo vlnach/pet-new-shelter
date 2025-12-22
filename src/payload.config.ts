f08ada61bf23b3f7aacd864871b2c9aac4993060
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-12-23 00:34:31 +0100

    Update dependencies and integrate S3 storage adapter
    - Bump @payloadcms/next to version 3.69.0 in package.json.
    - Import S3ClientUploadHandler from @payloadcms/storage-s3/client in importMap.js.
    - Configure S3 storage adapter in payload.config.ts with necessary credentials and settings for media collection.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,7 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -37,6 +38,21 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET as string,
+      config: {
+        endpoint: process.env.S3_ENDPOINT as string,
+        region: process.env.S3_REGION || 'auto',
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+        },
+
+        forcePathStyle: true,
+      },
+    }),
   ],
 })
